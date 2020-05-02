a3c1aa1498eb1e4bfcae22404d667e5e88619e95
Ian <ianfreitas1@poli.ufrj.br>
2020-05-02 20:27:34 -0300

    Added login session

--- nodejs/src/routes.js
+++ nodejs/src/routes.js
@@ -1,16 +1,24 @@
 import { Router } from 'express';
+import authMiddleware from './app/middlewares/auth';
 import ProductController from './app/controllers/ProductController';
 import ProductDetailController from './app/controllers/ProductDetailController';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 
+routes.post('/login', SessionController.store);
+
 routes.get('/products', ProductController.index);
-routes.post('/products', ProductController.store);
 
 routes.get('/products/:id', ProductDetailController.index);
+
+routes.use(authMiddleware);
+
+routes.post('/products', ProductController.store);
+
 routes.put('/products/:id', ProductDetailController.update);
 routes.delete('/products/:id', ProductDetailController.delete);
 
