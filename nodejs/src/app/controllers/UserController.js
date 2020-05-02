28c5e09aba4475974c82940002386eef1d416d4a
Ian <ianfreitas1@poli.ufrj.br>
2020-05-02 20:02:09 -0300

    Added create user api

--- nodejs/src/app/controllers/UserController.js
+++ nodejs/src/app/controllers/UserController.js
@@ -1,8 +1,35 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
-  async index(req, res) {
-    return res.json();
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string().required().min(6),
+      address: Yup.string().notRequired(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Invalid request body.' });
+    }
+
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ error: 'User already registered with this email. ' });
+    }
+
+    const { id, name, email, address } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      address,
+    });
   }
 }
 
--- nodejs/src/routes.js
+++ nodejs/src/routes.js
@@ -5,13 +5,13 @@ import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
+routes.post('/users', UserController.store);
+
 routes.get('/products', ProductController.index);
 routes.post('/products', ProductController.store);
 
 routes.get('/products/:id', ProductDetailController.index);
 routes.put('/products/:id', ProductDetailController.update);
 routes.delete('/products/:id', ProductDetailController.delete);
-
-routes.get('/users', UserController.index);
 
 export default routes;
