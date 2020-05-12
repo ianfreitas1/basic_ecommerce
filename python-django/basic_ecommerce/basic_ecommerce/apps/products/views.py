64cd56cf8d1d455d9a17729a30d5ad425eb4da78
Ian Freitas <ianfreitas1@poli.ufrj.br>
2020-05-12 19:50:06 -0300

    Added products basic CRUD

--- python-django/basic_ecommerce/basic_ecommerce/apps/products/views.py
+++ python-django/basic_ecommerce/basic_ecommerce/apps/products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [AllowAny(), ]
+        return super(ProductViewSet, self).get_permissions()
--- python-django/basic_ecommerce/basic_ecommerce/urls.py
+++ python-django/basic_ecommerce/basic_ecommerce/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('basic_ecommerce.apps.users.urls'))
+    path('', include('basic_ecommerce.apps.users.urls')),
+    path('', include('basic_ecommerce.apps.products.urls'))
 ]
