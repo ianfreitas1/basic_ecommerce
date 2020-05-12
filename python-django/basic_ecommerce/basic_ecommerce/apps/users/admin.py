2e9034b7f436688650e276c572c9a926597d8341
Ian Freitas <ianfreitas1@poli.ufrj.br>
2020-05-12 19:22:26 -0300

    Added admin display

--- python-django/basic_ecommerce/basic_ecommerce/apps/users/admin.py
+++ python-django/basic_ecommerce/basic_ecommerce/apps/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from basic_ecommerce.apps.users.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ('username', 'first_name', 'last_name', 'email', 'address')
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email')
+
+
+admin.site.register(User, UserAdmin)
