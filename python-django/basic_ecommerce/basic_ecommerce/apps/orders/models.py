ab6721396746d559ba6359f3266221d0f7322b8b
Ian Freitas <ianfreitas1@poli.ufrj.br>
2020-05-14 20:31:37 -0300

    Removed field total_price from orders model

--- python-django/basic_ecommerce/basic_ecommerce/apps/orders/models.py
+++ python-django/basic_ecommerce/basic_ecommerce/apps/orders/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.core.validators import MinValueValidator
+from django.db import models
 
 from basic_ecommerce.apps.products.models import Product
 from basic_ecommerce.apps.users.models import User
@@ -7,7 +7,6 @@ from basic_ecommerce.apps.users.models import User
 
 class Order(models.Model):
     item_quantity = models.IntegerField(validators=[MinValueValidator(0)])
-    total_price = models.FloatField(validators=[MinValueValidator(0)])
     paid = models.BooleanField(default=False)
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='orders')
