c5f079c76886058d9d674d1d30dee27ffb6d69be
Ian Freitas <ianfreitas1@poli.ufrj.br>
2020-05-16 19:05:46 -0300

    Added admin display

--- python-django/basic_ecommerce/basic_ecommerce/apps/orders/admin.py
+++ python-django/basic_ecommerce/basic_ecommerce/apps/orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'product', 'item_quantity', 'paid')
+
+
+admin.site.register(Order, OrderAdmin)
