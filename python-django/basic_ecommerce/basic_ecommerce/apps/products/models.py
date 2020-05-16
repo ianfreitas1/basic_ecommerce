e9fcbbc0a7ed1fd9425be0b1dd02745c8acd6867
Ian Freitas <ianfreitas1@poli.ufrj.br>
2020-05-16 19:07:50 -0300

    Added product str representation

--- python-django/basic_ecommerce/basic_ecommerce/apps/products/models.py
+++ python-django/basic_ecommerce/basic_ecommerce/apps/products/models.py
@@ -12,3 +12,6 @@ class Product(models.Model):
         db_table = "products"
         verbose_name = "Product"
         verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
